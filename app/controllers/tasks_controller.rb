4fa9854a94bd82b0dddbe100c8a81d50a48704d1
Joana Cruz <joana.if.cruz@gmail.com>
2022-08-16 14:57:21 +0100

    refactored find with before_action in controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,17 +19,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.delete
     redirect_to tasks_path, status: :see_other
   end
@@ -37,6 +35,10 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
